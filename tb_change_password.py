c5ef2fa20c5cf316c601857a5026c2ba30de0e49
folger <folger6@gmail.com>
2014-10-07 11:16:07 +0800

    polish tb_change_password

--- tb_change_password.py
+++ tb_change_password.py
@@ -8,29 +8,38 @@ from selenium.common.exceptions import TimeoutException, WebDriverException
 
 def wait(wait_func):
     try:
-        WebDriverWait(driver, 10).until(wait_func)
+        WebDriverWait(driver, 15).until(wait_func)
+        return True
     except TimeoutException:
-        print('time out')
+        return False
 
-with open('newpassword.txt') as f:
-    newpassword = f.read().strip()
+with open('config.txt') as f:
+    source, newpassword = f.read().strip().split('\n')[:2]
 
-with open('1.txt') as f:
+with open(source) as f:
     for i, line in enumerate(f):
-        if i % 5 == 0:
-            sleep(60)
+        #if i > 0 and i % 5 == 0:
+            #sleep(30)
 
+        line = line.strip()
         username, password = line.split('|')[:2]
         print(str(i + 1), username)
 
         driver = webdriver.Chrome('./chromedriver')
-        driver.get('https://login.taobao.com/member/login.jhtml')
-        wait(lambda the_driver: the_driver.find_element_by_id('TPL_username_1'))
+        while True:
+            driver.get('https://login.taobao.com/member/login.jhtml')
+            if wait(lambda the_driver: the_driver.find_element_by_id('TPL_username_1')):
+                break
+
         driver.find_element_by_id('TPL_username_1').send_keys(username)
         driver.find_element_by_id('TPL_password_1').send_keys(password)
         driver.find_element_by_id('J_SubmitStatic').click()
 
-        wait(lambda the_driver: the_driver.find_element_by_id('J_MtMainNav'))
+        if not wait(lambda the_driver: the_driver.find_element_by_id('J_MtMainNav')):
+            driver.quit()
+            with open('fail.txt', 'a') as f:
+                print(line, file=f)
+            continue
 
         driver.get('http://member.1688.com/member/account_security.htm')
 
@@ -57,7 +66,7 @@ with open('1.txt') as f:
             .find_elements_by_tag_name('td')[1]\
             .find_element_by_tag_name('input').click()
 
-        sleep(5)
+        sleep(3)
         driver.quit()
 
 print('All Done')
