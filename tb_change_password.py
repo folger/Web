3211f98a358caefacbba3ff1d4054ef9b9cb72c2
folger <folger6@gmail.com>
2014-10-08 22:07:27 +0800

    update tb_change_password

--- tb_change_password.py
+++ tb_change_password.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, WebDriverException
 
 
+class WrongPassword(Exception): pass
+class NotAlibaba(Exception): pass
+
 def wait(wait_func):
     try:
         WebDriverWait(driver, 15).until(wait_func)
@@ -18,55 +21,64 @@ with open('config.txt') as f:
 
 with open(source) as f:
     for i, line in enumerate(f):
-        #if i > 0 and i % 5 == 0:
-            #sleep(30)
-
         line = line.strip()
+        if len(line) == 0:
+            continue
+
         username, password = line.split('|')[:2]
         print(str(i + 1), username)
 
-        driver = webdriver.Chrome('./chromedriver')
-        while True:
-            driver.get('https://login.taobao.com/member/login.jhtml')
-            if wait(lambda the_driver: the_driver.find_element_by_id('TPL_username_1')):
-                break
-
-        driver.find_element_by_id('TPL_username_1').send_keys(username)
-        driver.find_element_by_id('TPL_password_1').send_keys(password)
-        driver.find_element_by_id('J_SubmitStatic').click()
-
-        if not wait(lambda the_driver: the_driver.find_element_by_id('J_MtMainNav')):
-            driver.quit()
-            with open('fail.txt', 'a') as f:
-                print(line, file=f)
-            continue
-
-        driver.get('http://member.1688.com/member/account_security.htm')
-
-        wait(lambda the_driver: the_driver.find_element_by_id('security_product'))
-        driver.find_element_by_id('security_product')\
-            .find_element_by_tag_name('table')\
-            .find_element_by_tag_name('tbody')\
-            .find_elements_by_tag_name('tr')[1]\
-            .find_elements_by_tag_name('td')[3]\
-            .find_element_by_tag_name('a').click()
-
-        wait(lambda the_driver: the_driver.find_element_by_id('oldPassword'))
-        driver.find_element_by_id('oldPassword').send_keys(password)
         try:
-            driver.find_element_by_id('newPassword').send_keys(newpassword)
+            driver = webdriver.Chrome('./chromedriver')
+            while True:
+                driver.get('https://login.taobao.com/member/login.jhtml')
+                if wait(lambda the_driver: the_driver.find_element_by_id('TPL_username_1')):
+                    break
+
+            driver.find_element_by_id('TPL_username_1').send_keys(username)
+            driver.find_element_by_id('TPL_password_1').send_keys(password)
+            driver.find_element_by_id('J_SubmitStatic').click()
+
+            if not wait(lambda the_driver: the_driver.find_element_by_id('J_MtMainNav')):
+                raise WrongPassword
+
+            driver.get('http://member.1688.com/member/account_security.htm')
+
+            wait(lambda the_driver: the_driver.find_element_by_id('security_product'))
+            driver.find_element_by_id('security_product')\
+                .find_element_by_tag_name('table')\
+                .find_element_by_tag_name('tbody')\
+                .find_elements_by_tag_name('tr')[1]\
+                .find_elements_by_tag_name('td')[3]\
+                .find_element_by_tag_name('a').click()
+
+            if not wait(lambda the_driver: the_driver.find_element_by_id('oldPassword')):
+                raise NotAlibaba
+
+            driver.find_element_by_id('oldPassword').send_keys(password)
+            try:
+                driver.find_element_by_id('newPassword').send_keys(newpassword)
+            except WebDriverException:
+                driver.find_element_by_id('newPassword').send_keys(newpassword)
+
+            driver.find_element_by_id('confirmPassword').send_keys(newpassword)
+            driver.find_element_by_id('modifyPassword')\
+                .find_element_by_tag_name('table')\
+                .find_element_by_tag_name('tbody')\
+                .find_elements_by_tag_name('tr')[3]\
+                .find_elements_by_tag_name('td')[1]\
+                .find_element_by_tag_name('input').click()
+        except WrongPassword:
+            with open('WrongPassword.txt', 'a') as f:
+                print(line, file=f)
+        except NotAlibaba:
+            with open('NotAlibaba.txt', 'a') as f:
+                print(line, file=f)
         except WebDriverException:
-            driver.find_element_by_id('newPassword').send_keys(newpassword)
-
-        driver.find_element_by_id('confirmPassword').send_keys(newpassword)
-        driver.find_element_by_id('modifyPassword')\
-            .find_element_by_tag_name('table')\
-            .find_element_by_tag_name('tbody')\
-            .find_elements_by_tag_name('tr')[3]\
-            .find_elements_by_tag_name('td')[1]\
-            .find_element_by_tag_name('input').click()
-
-        sleep(3)
-        driver.quit()
+            with open('OtherFail.txt', 'a') as f:
+                print(line, file=f)
+        finally:
+            sleep(1)
+            driver.quit()
 
 print('All Done')
